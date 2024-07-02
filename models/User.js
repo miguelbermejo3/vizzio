bb89bf68f2da42ca746dcdad1bcc3e036cbbc07e
miguelbermejo3 <118432695+miguelbermejo3@users.noreply.github.com>
2024-07-02 14:23:47 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+    nombre:{type:string},
+    apellido:{type:string},
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
 });
